dc756f44193c96738ced55295d69289d7cb7f555
Basabe-Gabriel <92063962+Basabe-Gabriel@users.noreply.github.com>
2021-11-04 12:20:27 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+let user = document.getElementById("Useringresado");
+let pass = document.getElementById("Passingresado");
+
+Aceptar.addEventListener("click", () => {
+
+  let user = document.getElementById("Useringresado");
+  let pass = document.getElementById("Passingresado");
+  
+  Aceptar.addEventListener("click", () => {
+  let user: string = "Gabriel";
+  let pass: string = "Basabe18..21";
+  
+  let useringresado: string = dato1.value;
+  let passingresado: string = dato2.value;
+  
+  if (user === useringresado && pass === passingresado) {
+    console.log("Bienvenido");
+  } else {
+    console.log("Fuerra intruso");
+     }
+  }
+}
